9d2948df7506d4d31664fc31dca109fb52a538d2
Nhatminhhmu <103763861+Nhatminhhmu@users.noreply.github.com>
2026-08-25 00:47:44 +0700

    Delete parsers/hfma.py

--- parsers/hfma.py
+++
@@ -1,4 +0,0 @@
-from ._common import collect_listing
-
-def collect(source):
-    return collect_listing(source, allowed=('/content-type/news/',), blocked=['/membership', '/events', '/about'], max_items=35)
