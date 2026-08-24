5d4df558f85c9d3675e5c8b1813fdadf6c6a8698
Nhatminhhmu <103763861+Nhatminhhmu@users.noreply.github.com>
2026-08-25 00:48:04 +0700

    Delete parsers/healthit_gov.py

--- parsers/healthit_gov.py
+++
@@ -1,4 +0,0 @@
-from ._common import collect_listing
-
-def collect(source):
-    return collect_listing(source, allowed=['/news/', '/blog/'], blocked=('/about', '/topics', '/resources'), max_items=35)
