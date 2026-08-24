42a21b14f5f6422b54ecab6e2f97579125c4bded
Nhatminhhmu <103763861+Nhatminhhmu@users.noreply.github.com>
2026-08-25 00:47:55 +0700

    Delete parsers/healthleaders.py

--- parsers/healthleaders.py
+++
@@ -1,4 +0,0 @@
-from ._common import collect_listing
-
-def collect(source):
-    return collect_listing(source, allowed=('/',), blocked=('/webinar', '/events', '/podcast', '/newsletter', '/about'), max_items=35)
