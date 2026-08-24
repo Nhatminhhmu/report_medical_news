e8116278046aaa44eb6b92f472b64b0fab401b5d
Nhatminhhmu <103763861+Nhatminhhmu@users.noreply.github.com>
2026-08-25 00:53:16 +0700

    Delete parsers/healthcare_dive.py

--- parsers/healthcare_dive.py
+++
@@ -1,4 +0,0 @@
-from ._common import collect_listing
-
-def collect(source):
-    return collect_listing(source, allowed=('/news/', '/'), blocked=('/events', '/about', '/podcasts', '/webinars'), max_items=35)
