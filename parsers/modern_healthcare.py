a74fe35a49ada41e03e5067e529f834e6f9a8616
Nhatminhhmu <103763861+Nhatminhhmu@users.noreply.github.com>
2026-08-25 00:46:46 +0700

    Delete parsers/modern_healthcare.py

--- parsers/modern_healthcare.py
+++
@@ -1,4 +0,0 @@
-from ._common import collect_listing
-
-def collect(source):
-    return collect_listing(source, allowed=('/',), blocked=('/podcast', '/video', '/events', '/jobs', '/about'), max_items=35)
