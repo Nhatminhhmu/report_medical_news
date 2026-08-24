aacfe56149b5837ecb8c4e171fea9d6ed5b98384
Nhatminhhmu <103763861+Nhatminhhmu@users.noreply.github.com>
2026-08-25 00:46:38 +0700

    Delete parsers/fierce_healthcare.py

--- parsers/fierce_healthcare.py
+++
@@ -1,4 +0,0 @@
-from ._common import collect_listing
-
-def collect(source):
-    return collect_listing(source, allowed=('/',), blocked=('/rss', '/events', '/webinars', '/podcasts', '/about'), max_items=35)
